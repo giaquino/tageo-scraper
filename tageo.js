027fa59907b95cdea247e0a4195bc849642bb04e
Gian Darren Azriel Aquino <gda.i.aquino@gmail.com>
2017-02-02 08:49:54 +0800

    refactor

--- tageo.js
+++ tageo.js
@@ -1,24 +1,14 @@
-/*
-Copyright 2017 Gian Darren Azriel Aquino
+/**
+ * Scrapes tageo site to get csv for `city`, `lat`, `lng`, `geohash`
+ *
+ * How to use:
+ * run `npm install`
+ * run `node tageo.js tageo_url_to_cities`
+ */
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 var request = require('request');
 var cheerio = require('cheerio');
 var geohash = require('geo-hash');
-
-//scrapes tageo url to get `city`, `lat`, `lng`, `geohash`
-//how to use: node tageo.js tageo_url_to_cities
 
 var url = process.argv[2];
 
